5bcaf714c38ce02158ad03d664ab8bd3536d868b
edoardo <collo>
2021-02-22 23:58:30 +0100

    added category seeder

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Category;
 class CategorySeeder extends Seeder
 {
     /**
@@ -11,6 +11,33 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+      $categories = [
+        [
+          'categoria'=>'piatti',
+          'descrizione'=>'oggetti per contenere il cibo'
+        ],
+        [
+          'categoria'=>'coltelli',
+          'descrizione'=>'oggetti per tagliare il cibo'
+        ],
+        [
+          'categoria'=>'bicchieri',
+          'descrizione'=>'oggetti per contenere liquidi'
+        ],
+        [
+          'categoria'=>'vasi',
+          'descrizione'=>'oggetti per contenere piante'
+        ],
+        [
+          'categoria'=>'contenitori',
+          'descrizione'=>'oggetti per contenere oggetti'
+        ]
+      ];
+      foreach ($categories as $category) {
+        $newCategory = new Category;
+        $newCategory->categoria = $category['categoria'];
+        $newCategory->descrizione = $category['descrizione'];
+        $newCategory->save();
+      }
     }
 }
